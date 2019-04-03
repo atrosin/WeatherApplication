0612dce418aeb5118a95018e3a10337ce11972b9
andretrosso <zuzik190394@gmail.com>
2019-04-03 23:15:30 +0300

    get inner objects from json

--- app/src/main/java/com/example/android/weatherapplication/MainActivity.java
+++ app/src/main/java/com/example/android/weatherapplication/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.android.weatherapplication;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +18,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NetworkService.getSingleInst()
+        .getCallPlace()
+        .getData("London",NetworkService.getKEY())
+        .enqueue(new Callback<SumResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<SumResponse> call, @NonNull Response<SumResponse> response) {
+                System.out.println(call.request().toString());
+                System.out.println(response.message()+"\n");
+                //SumResponse sumResponse = response.body();
+                ArrayList<SumResponse.TargetObj> arrayList = new ArrayList<>(response.body().getList());
+                System.out.println(arrayList.get(0).main.toString());
+
+            }
+
+            @Override
+            public void onFailure(Call<SumResponse> call, Throwable t) {
+                Log.e(this.toString(),"What a fuck "+call.request().toString()+" "+t);
+
+            }
+
+
+
+});
 
     }
 }
